9503b407bd2fee3264a923c5e2c50a7ad7f6ca0b
FrancoEdu <franc0edu24@gmail.com>
2023-01-21 15:10:26 -0300

    First steps using Javascript

--- moduleB - Firts steps using JavaScript/aula04/ex001js.js
+++ moduleB - Firts steps using JavaScript/aula04/ex001js.js
@@ -1,3 +1,3 @@
 window.alert("Minha primeira mensagem!"); //alerta
-window.confirm("Está gostando do curso?"); //indagação
+window.confirm("Está gostando do curso?"); //indagação, janela com botão OK e cancelar
 window.prompt("Qual seu nome?"); //mostra caixa de entrada de dados
