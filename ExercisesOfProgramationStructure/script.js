3423f417e50f1d7958341788805618fe882e3311
FrancoEdu <franc0edu24@gmail.com>
2023-01-24 20:59:40 -0300

    function idade to give salutation

--- ExercisesOfProgramationStructure/script.js
+++ ExercisesOfProgramationStructure/script.js
@@ -1,8 +1,6 @@
-let range = prompt("Defina um número limite:")
-
-numberPrime(range);
-
+//Função para calcular quais números saõ primos até um determiando limite
 function numberPrime(num){
+    let range = prompt("Defina um número limite:")
     console.log("Estes são os primos até o limite definido por você: ")
 
     for(let dividend = num; dividend >= 1; dividend--){
@@ -17,3 +15,6 @@ function numberPrime(num){
         }
     }
 }
+
+//Função de saudação utilizando arrow function
+let idade = (idade) => console.log(`Você tem ${idade} anos`)
