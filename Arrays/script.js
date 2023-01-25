9b2e1499e17f36187786cd5fad146ebde883e91b
FrancoEdu <franc0edu24@gmail.com>
2023-01-25 20:08:22 -0300

    Learn methods of Array, like, foreach, include, slice, reverse, shift and unshift

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,3 +1,27 @@
-let array = [1,5,8,11]
+let array = [1,5,8,11,5]
 let arrayOfString = ["Eduardo","Letícia","Julia"]
 let arrayOfBoolean = [true,false,true]
+
+//shift e unshift, -> shit exclui a primeira posição do array, unshift adiciona 
+//uma ou mais posições ao array
+// let carros = ["Onix","Palio","Astra"]
+// console.log(carros.shift())
+// console.log(carros.unshift("Gol"))
+// console.log(carros)
+
+//indexOf e lastIndexOf, -> indexOf encontra o indice de um determinado elemento
+//lastIndexOf -> encontra o ultimo indice de um elemento
+// console.log(array.indexOf(11))
+// console.log(array.lastIndexOf(5))
+
+//Slice -> retorna um array partindo de outro array, o inicio e fim são determinados
+//pelos paramentros
+// console.log(array.slice(1,11))
+
+//foreach -> itera cada elemento do array
+// array.forEach(element => {
+//     console.log("O numero é " + element)
+// });
+
+//includes -> Verifica se o array tem um determinado elemento, metodo booleano
+// console.log(arrayOfString.includes("Julia"))
